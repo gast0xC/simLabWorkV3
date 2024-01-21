f220fd6c1b9cf1bb8589bf0bb35d06a8a17931ab
loides <84963089+loides@users.noreply.github.com>
2024-01-21 15:54:03 +0000

    update register success

--- backend/mvc/user/views/registerSuccess.php
+++ backend/mvc/user/views/registerSuccess.php
@@ -8,17 +8,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login successful !</title>
+    <title>Register successful !</title>
     <!-- Add additional meta tags and CSS links here -->
 </head>
 <body>
     <h1>Register Successful!</h1>
-    <p>Welcome, <?php echo htmlspecialchars($_SESSION['name']); ?>!</p>
-    <p>You will be redirected to your dashboard shortly.</p>
     <p>Please login afterwards.</p>
     <script>
         setTimeout(function() {
-            window.location.href = '/webapp/app.php?service=showLayout'; // Redirect to a test page -----> CHANGE !!!!
+            window.location.href = '/webapp/app.php?service=showLayout#'; // Redirect to a test page -----> CHANGE !!!!
         }, 3000); // Redirect after 3 seconds
     </script>
 </body>
